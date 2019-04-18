11c41805118e4dc8ad4abdb2eac3f44d5aec8097
Mohamed Abdelrazik <mohamedabdelrazik188@gmail.com>
2019-04-18 15:44:35 +0200

    ss

--- app/src/main/java/com/farag/hamzamohamed/fragmentapp/MainActivity.java
+++ app/src/main/java/com/farag/hamzamohamed/fragmentapp/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Hello
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
